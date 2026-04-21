1fc106451ca3474deca77892f286dc43e2870671
Icarucci <ignaciocarucci@hotmail.com>
2026-04-21 20:18:47 -0300

    Aregar contacto diseño

--- app/src/main/java/com/roselli208carucci301/usermanagement/AgregarContactoActivity.java
+++ app/src/main/java/com/roselli208carucci301/usermanagement/AgregarContactoActivity.java
@@ -1,34 +1,41 @@
 package com.roselli208carucci301.usermanagement;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class AgregarContactoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_agregar_contacto);
+
+        ImageButton btnGuardarToolbar = findViewById(R.id.btnGuardarToolbar);
+
+        btnGuardarToolbar.setOnClickListener(view -> {
+            Toast.makeText(this, "Contacto guardado", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+
+        ImageButton btnAtrasToolbar = findViewById(R.id.btnAtrasToolbar);
+
+        btnAtrasToolbar.setOnClickListener(view -> {
+            finish();
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button btnIrAgregarContacto = findViewById(R.id.btnIrAgregarContacto);
-
-        btnIrAgregarContacto.setOnClickListener(view -> {
-            Intent intent = new Intent(HomeActivity.this, AgregarContactoActivity.class);
-            startActivity(intent);
         });
     }
 }
