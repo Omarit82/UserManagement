0b3ca5b42ef1c606c65960ec6242fcf59955b1d8
Omarit82 <roselliomar82@gmail.com>
2026-04-21 10:53:25 -0300

    Login, definida la paleta de colores

--- app/src/main/java/com/roselli208carucci301/usermanagement/LoginActivity.java
+++ app/src/main/java/com/roselli208carucci301/usermanagement/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.roselli208carucci301.usermanagement;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button init = findViewById(R.id.init);
+
+        init.setOnClickListener(v -> {
+            EditText user = findViewById(R.id.email);
+            EditText pass = findViewById(R.id.password);
+            if(user.getText().toString().equals("admin@admin.com") && pass.getText().toString().equals("1234")){
+                Toast.makeText(LoginActivity.this,"Logged in",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(LoginActivity.this,"Error",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
